81b78e28ec9dcbf98fdc1aa845bb80464bf0f714
hectorphelma <47922985+hectorphelma@users.noreply.github.com>
2019-04-30 20:04:43 +0200

    Update graphe.h

--- include/graphe.h
+++ include/graphe.h
@@ -1,11 +1,11 @@
 //structure
 
-
-typedef struct { char* nom; char* line; double x,y ; L_ARC voisins; int ListeFermee} T_SOMMET ; //avec x,y les coordonnées et ListeFermee un booleen qui donne l'appartenance à la liste fermée du sommet
-
 typedef struct { int arrivee; double cout; } T_ARC ;
 
 typedef struct lsucc { T_ARC val; struct lsucc* suiv ; }* L_ARC; //liste des suivants immédiats
+
+typedef struct { char* nom; char* line; double x,y ; L_ARC voisins; int ListeFermee} T_SOMMET ; //avec x,y les coordonnées et ListeFermee un booleen qui donne l'appartenance à la liste fermée du sommet
+
 
 
 
@@ -13,9 +13,13 @@ typedef struct lsucc { T_ARC val; struct lsucc* suiv ; }* L_ARC; //liste des sui
 
 T_SOMMET* creation_graphe(int nbsommet);
 void creation_sommet(T_SOMMET* psommet,char* nom,char* line, double longi, double lat);
-void suppression_graphe(T_SOMMET graphe,nbsommet);
 void creation_arc(int numdepart, int numarrivee, double cout,T_SOMMET* graphe);
-T_SOMMET* ouvrir_fichier(char* nomfichier);
+void suppression_graphe(T_SOMMET graphe,nbsommet);
+void affiche_arc(L_ARC voisins)
+void afficher_graphe(T_SOMMET* graphe,int nbsommet)
+void creation_arc(int numdepart, int numarrivee, double cout,T_SOMMET* graphe)
+
+T_SOMMET* ouvrir_fichier(char* nomfichier,int* pnbsommet);
 
 
 
