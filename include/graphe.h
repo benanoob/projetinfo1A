a30cf82187704a25ac688b79912a5551ab6c35b2
hectorphelma <47922985+hectorphelma@users.noreply.github.com>
2019-05-01 12:07:43 +0200

    Update graphe.h

--- include/graphe.h
+++ include/graphe.h
@@ -14,7 +14,7 @@ typedef struct { char* nom; char* line; double x,y ; L_ARC voisins; int ListeFer
 T_SOMMET* creation_graphe(int nbsommet);
 void creation_sommet(T_SOMMET* psommet,char* nom,char* line, double longi, double lat);
 void creation_arc(int numdepart, int numarrivee, double cout,T_SOMMET* graphe);
-void suppression_graphe(T_SOMMET graphe,nbsommet);
+void suppression_graphe(T_SOMMET* graphe,int nbsommet);
 void affiche_arc(L_ARC voisins)
 void afficher_graphe(T_SOMMET* graphe,int nbsommet)
 void creation_arc(int numdepart, int numarrivee, double cout,T_SOMMET* graphe)
