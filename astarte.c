d9d6e33fbe09795bc18a0117e3a307f8a500b4e5
hectorphelma <47922985+hectorphelma@users.noreply.github.com>
2019-05-19 16:46:59 +0200

    Update astarte.c

--- astarte.c
+++ astarte.c
@@ -1,10 +1,12 @@
-#include <math.h>
+﻿#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "tas.h"
 #include "graphe.h"
 
+
+//on a besoin de savoir si un sommet est présetn dans un tas
 int cherche_dans_tas(T_SOMMET** tas,T_SOMMET* graphe,int search,int n_tas,int* posdanstas ){
   int i=0;
   for (i=0;i<n_tas;i++){
@@ -15,6 +17,7 @@ int cherche_dans_tas(T_SOMMET** tas,T_SOMMET* graphe,int search,int n_tas,int* p
   return(0);// n'est pas present
 }
 
+// evalue H
 double H(int sommets, int sommeta, T_SOMMET* graphe){
   double xa, xs, ya, ys;
   xa=(graphe+sommeta)->x;
@@ -25,6 +28,7 @@ double H(int sommets, int sommeta, T_SOMMET* graphe){
   return(sqrt(norme));
 }
 
+//revoie le cout pour aller d'un sommet à arrivée dans le cas où arrivée est son voisin ou bien -1 si arrivée n'est pas voisin de départ
 double cout(int depart,int arrivee,T_SOMMET* graphe){
   L_ARC arc=(graphe+depart)->voisins;
   while(arc!=NULL){
@@ -35,6 +39,8 @@ double cout(int depart,int arrivee,T_SOMMET* graphe){
   return(-1);
 }
 
+
+//initialise le tableau dans lequel on crée le tas afin que tous les sommets* du graphe y soient déjà pointés
 void init_tas(T_SOMMET** tas,T_SOMMET* graphe,int nbsommet){
   int i=0;
   for (i=0;i<nbsommet;i++){
@@ -44,6 +50,7 @@ void init_tas(T_SOMMET** tas,T_SOMMET* graphe,int nbsommet){
 }
 
 
+//cherche un sommet dans le graphe par son nom et en retourne le numero 
 int trouver_sommet(char* depart,T_SOMMET* graphe,int nbsommet){
   int a=0;
   while(a<nbsommet){
@@ -53,6 +60,7 @@ int trouver_sommet(char* depart,T_SOMMET* graphe,int nbsommet){
     a+=1;
   }
 return(nbsommet+1);}
+
 
 int main(){
   //initialisation
@@ -109,7 +117,11 @@ int main(){
               *(tas+(k+1)/2)=graphe+s;
               *(tas+posdanstas)=stock;//Pere(s)←k
               (graphe+k)->F=((graphe+k)->F)-H(k, a, graphe)+ cout(k,s,graphe)+ H(s,a,graphe);
-              n_tas=max(n_tas,posdanstas);
+              stock=*(tas+n_tas+1);
+              *(tas+n_tas+1)=*(tas+posdanstas);
+              *(tas+posdanstas)=stock;//on a inséré le nouveau sommet dans le tas
+		n_tas=max(n_tas,posdanstas);
+		n_tas=n_tas+1;
               augmentetas(tas,n_tas);
             }
 
@@ -118,13 +130,33 @@ int main(){
                 T_SOMMET* stock=*(tas+(k+1)/2);
                 *(tas+(k+1)/2)=graphe+s;
                 *(tas+posdanstas)=stock;//Pere(s)←k
+		supprimeSommetTas(tas,posdanstas);
+		(*(tas+posdanstas))->F;
+		
+              	(graphe+k)->F=((graphe+k)->F)-H(k, a, graphe)+ cout(k,s,graphe)+ H(s,a,graphe);
+		n_tas=n_tas+1;
+		augmenteTas(tas,n_tas);
+		
               }
             }
           }
           arc=arc->suiv;
         }
       }
+}
 
+void affiche_chemin(tas tas,int nbsommet){
+	int i=0;
+	double cout=0
+	while(i!=1){
+		printf("on passe par %s \n",(*(tas+i))->nom);
+		cout+=(*(tas+i))->cout;
+		if(2*(i+1)-1>=nbsommet){
+			printf("vous voilà arrivé\n");
+			i=1;}
+		i=2*(i+1)-1;
+	}
+	printf("le cout est alors de %lf \n",cout);
+	} 
 
-
-    }
+    
